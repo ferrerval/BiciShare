11b44c40db157ebaff8e0397193e08699232a055
Santiago-3023usc <Santiago.isaza00@usc.edu.co>
2025-09-12 00:38:53 -0500

    Creacion clase Prestamo

---
+++ ClasesBiciShare/Prestamo.java
@@ -0,0 +1,20 @@
+package ClasesBiciShare;
+
+public class Prestamo {
+    private Usuario usuario;
+    private Bicicleta bici
+
+    public Prestamo(Usuario usuario, Bicicleta bici){
+        this.usuario = usuario;
+        this.bici = bici;
+    }
+
+    public Usuario getUsuario(){
+        return = usuario;
+    }
+
+    public Bicicleta getBici(){
+        return = bici;
+    }
+
+}
