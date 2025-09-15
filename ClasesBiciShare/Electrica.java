55aa0a082a0e15028d7d27a297fc189fa181a490
Santiago3023-usc <santiago.isaza00@usc.edu.co>
2025-09-15 00:47:50 -0500

    comentarios de la clase Mecanica y Electrica

--- ClasesBiciShare/Electrica.java
+++ ClasesBiciShare/Electrica.java
@@ -1,18 +1,23 @@
 package ClasesBiciShare;
 
+//Hereda de Bicicleta e incluye un atributo extra: nivelBateria
 public class Electrica extends Bicicleta {
-
+    // porcentaje de batería de la bicicleta (0 a 100)
     private int nivelBateria;
 
+    // Constructor: crea una bici eléctrica con id, estado y nivel de batería
     public Electrica(int id, String estado, int nivelBateria) {
         super(id, estado);
         this.nivelBateria = nivelBateria;
     }
 
+    // Métodos para consultar o actualizar el nivel de batería.
+    // Devuelve el nivel de batería actual
     public int getNivelBateria() {
         return nivelBateria;
     }
 
+    // Permite cambiar el nivel de batería (al recargar o descargar)
     public void setNivelBateria(int nivelBateria) {
         this.nivelBateria = nivelBateria;
     }
