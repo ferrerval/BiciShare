e287316481c7d6eaa0b3acc9a778dbf9e89a2f0c
Santiago3023-usc <santiago.isaza00@usc.edu.co>
2025-09-15 00:50:06 -0500

    comentarios clase Prestamo

--- ClasesBiciShare/Prestamo.java
+++ ClasesBiciShare/Prestamo.java
@@ -1,19 +1,25 @@
 package ClasesBiciShare;
 
 public class Prestamo {
+    // Usuario que tiene la bicicleta prestada
     private Usuario usuario;
+    // Bicicleta que el usuario tomó en préstamo
     private Bicicleta bici;
 
-    public Prestamo(Usuario usuario, Bicicleta bici){
+    // Crea un nuevo préstamo ligando un usuario con una bicicleta.
+    // Se usa cuando el usuario toma una bicicleta
+    public Prestamo(Usuario usuario, Bicicleta bici) {
         this.usuario = usuario;
         this.bici = bici;
     }
 
-    public Usuario getUsuario(){
+    // Métodos para consultar el usuario o la bicicleta de este préstamo.
+    // Se usan para mostrar el historial y para las devoluciones.
+    public Usuario getUsuario() {
         return usuario;
     }
 
-    public Bicicleta getBici(){
+    public Bicicleta getBici() {
         return bici;
     }
 
