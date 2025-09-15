6f56d5e884684e296b7ddba55e39119904ce41b3
Santiago3023-usc <santiago.isaza00@usc.edu.co>
2025-09-15 00:33:01 -0500

    correcion comentarios de usuario

--- ClasesBiciShare/Usuario.java
+++ ClasesBiciShare/Usuario.java
@@ -1,17 +1,12 @@
 package ClasesBiciShare;
 
 public class Usuario {
-    // nombre completo del usuario
+
     private String nombre;
-    // id que identifique al usuario
     private int id;
-    // indica si el usuario es Estudiante o Profesor
     private String TipoUsuario;
 
-    /**
-     * metodo constructor del usuario
-     * permite crear un usuario nuevo con su nombre, id y su tipo
-     */
+    // metodo constructor que recibe la informacion del nuevo usuario
 
     public Usuario(String nombre, int id, String TipoUsuario) {
         this.nombre = nombre;
@@ -19,17 +14,16 @@ public class Usuario {
         this.TipoUsuario = TipoUsuario;
     }
 
-    // retorna el id del usuario
+    // metodos para obtener los datos del usuario por el encapsulamiento, usados en
+    // todo el sistema
     public int getId() {
         return id;
     }
 
-    // retorna el nombre completo del usuario
     public String getNombre() {
         return nombre;
     }
 
-    // retorna si el usuario es Estudiante o Profesor
     public String getTipoUsuario() {
         return TipoUsuario;
     }
